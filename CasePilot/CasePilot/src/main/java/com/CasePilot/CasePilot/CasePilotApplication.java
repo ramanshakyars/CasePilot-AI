d461188b34fb51aaf56b04ff1e72959eb30549b9
ramanshakyars <ramanshakyars12@gmail.com>
2025-08-25 15:15:27 +0530

    first commit

---
+++ CasePilot/CasePilot/src/main/java/com/CasePilot/CasePilot/CasePilotApplication.java
@@ -0,0 +1,13 @@
+package com.CasePilot.CasePilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasePilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasePilotApplication.class, args);
+	}
+
+}
---
+++ CasePilot/CasePilot/src/test/java/com/CasePilot/CasePilot/CasePilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CasePilot.CasePilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasePilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
